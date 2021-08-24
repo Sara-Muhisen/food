4a5006e8ea581d40ea5a7d5a34dbb165ff5ef41e
sara <sara@gmail.com>
2021-08-24 21:26:56 +0300

    good

---
+++ js/java.js
@@ -0,0 +1,17 @@
+var header = document.getElementById("forAfter");
+var btns = header.getElementsByClassName("link");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {      
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+    });
+}
+
+window.onscroll = function(){
+    if( window.scrollY >= 200 ){
+    header.style.backgroundColor = "green";
+    }
+}
